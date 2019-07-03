a24aa7ec693e0152ffb4503ad8c4872fcabf0085
Lo Chenchou <lochenchou@users.noreply.github.com>
2019-07-03 15:26:50 +0800

    Fix error in download.sh
    Fix error in download.sh

--- data/download.sh
+++ data/download.sh
@@ -3,14 +3,15 @@ wget -O vcc2018_listening_test_scores.zip "https://datashare.is.ed.ac.uk/bitstre
 unzip vcc2018_listening_test_scores.zip
 rm vcc2018_listening_test_scores.zip
 
-download submitted_systems_converted_speech
+# download submitted_systems_converted_speech
 wget -O vcc2018_submitted_systems_converted_speech.tar.gz "https://datashare.is.ed.ac.uk/bitstream/handle/10283/3061/vcc2018_submitted_systems_converted_speech.tar.gz?sequence=10&isAllowed=y" 
 tar zxvf vcc2018_submitted_systems_converted_speech.tar.gz
 rm vcc2018_submitted_systems_converted_speech.tar.gz
-rename dir
+
+# rename dir
 mkdir submit
 mv ./mnt/sysope/test_files/testVCC2/*.wav ./submit
 rm -r ./mnt
 
-downsample
+# downsample
 python downsample.py
