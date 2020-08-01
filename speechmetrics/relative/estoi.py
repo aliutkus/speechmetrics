c0390a8925689c7d5fcd8e1a0e357dd06a34f23c
Jonas Haag <jonas@lophus.org>
2020-08-01 09:37:25 +0200

    Add ESTOI

---
+++ speechmetrics/relative/estoi.py
@@ -0,0 +1,10 @@
+from . import STOI
+
+
+class ESTOI(STOI):
+    def __init__(self, *args, **kwargs):
+        super(ESTOI, self).__init__(*args, **kwargs, estoi=True)
+
+
+def load(window, hop=None):
+    return ESTOI(window, hop)
