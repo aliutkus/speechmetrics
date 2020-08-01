ba2772c14467e498ae8737071c7db646b6519331
Jonas Haag <jonas@lophus.org>
2020-08-01 09:35:50 +0200

    Add ESTOI

--- speechmetrics/relative/stoi.py
+++ speechmetrics/relative/stoi.py
@@ -2,16 +2,17 @@ from .. import Metric
 
 
 class STOI(Metric):
-    def __init__(self, window, hop=None):
+    def __init__(self, window, hop=None, estoi=False):
         super(STOI, self).__init__(name='STOI', window=window, hop=hop)
         self.mono = True
+        self.estoi = estoi
 
     def test_window(self, audios, rate):
         from pystoi.stoi import stoi
         if len(audios) != 2:
             raise ValueError('STOI needs a reference and a test signals.')
 
-        return {'stoi':stoi(audios[1], audios[0], rate, extended=False)}
+        return {'stoi':stoi(audios[1], audios[0], rate, extended=self.estoi)}
 
 
 def load(window, hop=None):
