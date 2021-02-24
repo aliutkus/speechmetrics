cb94572ec9955ccf11e3f8130ba1f97a2be7b22b
刘濠赫 <liuhaohe.7@bytedance.com>
2021-02-24 16:02:52 +0800

    Fix bug for bsseval

--- speechmetrics/relative/bsseval.py
+++ speechmetrics/relative/bsseval.py
@@ -12,8 +12,9 @@ class BSSEval(Metric):
         from museval.metrics import bss_eval
         if len(audios) != 2:
             raise ValueError('BSSEval needs a reference and a test signals.')
-        result = bss_eval(reference_sources=audios[1].T,
-                        estimated_sources=audios[0].T,
+        
+        result = bss_eval(reference_sources=audios[1][None,...], # shape: [nsrc, nsample, nchannels]
+                        estimated_sources=audios[0][None,...],
                         window=self.bss_window * rate,
                         hop=self.bss_hop * rate)
         return {'sdr': result[0], 'isr': result[1], 'sar': result[3]}
